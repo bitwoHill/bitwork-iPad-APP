1f9b2d7aff71fda84dd2b048d050e7c3b7f501ab
Tim Hill <timhill@Thomass-MacBook-Pro.local>
2013-10-31 15:50:15 +0100

    Initial Commit

---
+++ bitwork iPad APP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bitwork iPad APP
+//
+//  Created by Tim Hill on 31.10.13.
+//  Copyright (c) 2013 Tim Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
