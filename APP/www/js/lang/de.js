09a1c48ef79b9b12adf7c49944e57c76b3d26cc8
bitwork <hill@bitwork.net>
2013-11-28 16:06:17 +0100

    Updated Localization

--- APP/www/js/lang/de.js
+++ APP/www/js/lang/de.js
@@ -2,8 +2,8 @@ var i18n = {
 
     langCode: "de",
     strings: {
-        "months": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        "days": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        "months": ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+        "days": ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam"],
         "na" : "N/A",
 
         //Menu
@@ -15,31 +15,31 @@ var i18n = {
         "menu-info": "Infothek",
 
         "last-sync": "Letzte Synchronisierung am",
-        "toggle-nav": "Toggle navigation",
-        "user-message": "Angemeldet als",
+        "toggle-nav": "Navigation umschalten",
+        "user-message": "Angemeldet als ",
         "login-different-user": "Als anderer Benutzer anmelden",
 
         "subtitle": "Herzlich willkommen auf dem Atlas Copco Portal",
 
         //Calendar
         "kalender": "Kalender",
-        "last-sync-calendar": "Last calendar sync",
+        "last-sync-calendar": "Letzte Kalender Synchonisation",
         "ort": "Ort",
         "anfangszeit": "Anfangszeit",
         "endzeit": "Endzeit",
-        "calendar-add-phone": "Add to phone calendar",
-        "empty-calendar": "There are no calendar items to be displayed. Use above <i class='fa fa-refresh'></i> button to syncronise he calendar.",
+        "calendar-add-phone": "Zum Telefonkalender hinzufügen",
+        "empty-calendar": "Es wurden noch keine Events geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um Events zu laden.",
 
         //News
         "news": "News",
-        "last-sync-news": "Last news sync",
+        "last-sync-news": "Letzte News Synchonisation",
         "weiterlesen": "Weiterlesen",
-        "empty-news": "There are no news to be displayed. Use above <i class='fa fa-refresh'></i> button to syncronise he news.",
+        "empty-news": "Es wurden noch keine News geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um News zu laden.",
 
         //Links
         "wichtige-links": "Wichtige Links",
-        "last-sync-links": "Last links sync",
-        "empty-links": "There are no links to be displayed. Use above <i class='fa fa-refresh'></i> button to syncronise he links.",
+        "last-sync-links": "Letzte wichtige Links Synchonisation",
+        "empty-links": "Es wurden noch keine wichtige Links geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um wichtige Links zu laden.",
 
         //MPL
         "stammdaten": "Stammdaten",
@@ -53,7 +53,7 @@ var i18n = {
         "druck": "Druck",
         "antriebsleistung": "Antriebsleistung",
         "empty-mpl": "Es wurden noch keine Equipments / Sonstigen Produkte geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um  Equipments / Sonstigen Produkte zu laden.",
-        "last-sync-mpl": "Zuletzt synchronisiert",
+        "last-sync-mpl": "Letzte Stammdaten Synchonisation",
 
         //MPL Documents
         "dokumente": "Dokumente",
@@ -66,26 +66,26 @@ var i18n = {
         //MPL Produtgruppen
         "produktgruppen": "Produktgruppen",
         "empty-produktgroups": "Es wurden noch keine Produktgruppen geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um Produktgruppen zu laden.",
-        "last-sync-productgroups": "Zuletzt synchronisiert",
+        "last-sync-productgroups": "Letzte Produktgruppen Synchonisation",
         //MPL Produktfamilien
         "produktfamilien": "Produktfamilien",
         "empty-produktfamilies": "Es wurden noch keine Produktfamilien geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um Produktfamilien zu laden.",
-        "last-sync-productfamilies": "Zuletzt synchronisiert",
+        "last-sync-productfamilies": "Letzte Produktfamilien Synchonisation",
         //MPL Produktplattformen
         "produktplattformen": "Produktplattformen",
         "empty-productplatforms": "Es wurden noch keine Produktplattformen geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um Produktplattformen zu laden.",
-        "last-sync-productplatforms": "Zuletzt synchronisiert",
+        "last-sync-productplatforms": "Letzte Produktplattform Synchonisation",
         //MPL Produkt
         "produkt": "Produkt",
         "empty-products": "Es wurden noch keine Produkte geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um Produkte zu laden.",
-        "last-sync-products": "Zuletzt synchronisiert",
+        "last-sync-products": "Letzte Produkt Synchonisation",
 
         //InfoThek
         "infothek": "Infothek",
 
         //Contacts
         "contacts": "Telefonbuch",
-        "empty-contacts": "There are no contacts to be displayed. Use above <i class='fa fa-refresh'></i> button to syncronise the contact page.",
-        "last-sync-contacts": "Last contacts sync",
+        "empty-contacts": "Es wurden noch keine Kontakte geladen. Benutzen Sie den <i class='fa fa-refresh'></i> Synchronisier-Button um Kontakdaten zu laden.",
+        "last-sync-contacts": "Letzte Kontaktdaten Synchonisation",
     }
 }
