d58154bb6047c963b704a37088867c5d10b218b4
bitwork GmbH <info@bitwork.net>
2013-12-04 15:46:18 +0100

    phonegap upgrade

--- APP/platforms/ios/www/cordova_plugins.js
+++ APP/platforms/ios/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/InAppBrowser.js",
-        "id": "org.apache.cordova.inappbrowser.InAppBrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.contacts/www/contacts.js",
         "id": "org.apache.cordova.contacts.contacts",
         "clobbers": [
@@ -225,6 +218,13 @@ module.exports = [
         "id": "nl.x-services.plugins.calendar.Calendar",
         "clobbers": [
             "Calendar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/InAppBrowser.js",
+        "id": "org.apache.cordova.inappbrowser.InAppBrowser",
+        "clobbers": [
+            "window.open"
         ]
     }
 ]
