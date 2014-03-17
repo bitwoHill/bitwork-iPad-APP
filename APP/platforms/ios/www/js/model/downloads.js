9e171c002bfbfd4f0c954e68857a625402940257
bitwork GmbH <info@bitwork.net>
2014-03-17 20:09:14 +0100

    Behandlung von 404 Fehlern beim Dateidownload

--- APP/platforms/ios/www/js/model/downloads.js
+++ APP/platforms/ios/www/js/model/downloads.js
@@ -1,69 +1,53 @@
 var DownloadModel = {};
 
-(function ($) {
-    DownloadModel.deleteAllFolders = function () {
-        var def = $.Deferred(),
-            removeDir1 = DownloadModel.removeDir("Dokumente"),
-            removeDir2 = DownloadModel.removeDir("Infothek");
+(function($) {
+    DownloadModel.deleteAllFolders = function() {
+        var def = $.Deferred(), removeDir1 = DownloadModel.removeDir("Dokumente"), removeDir2 = DownloadModel.removeDir("Infothek");
 
-        $.when(removeDir1, removeDir2)
-            .done(
-                function (rem1, rem2) {
-                    def.resolve()
-                }
-            ).fail(
-                function (rem1, rem2) {
-                    def.reject();
-                }
-            );
+        $.when(removeDir1, removeDir2).done(function(rem1, rem2) {
+            def.resolve()
+        }).fail(function(rem1, rem2) {
+            def.reject();
+        });
 
         return def.promise();
     };
 
-    DownloadModel.removeDir = function (dirName) {
-        var root,
-            def = $.Deferred();
+    DownloadModel.removeDir = function(dirName) {
+        var root, def = $.Deferred();
 
         try {
             window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
 
-            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
-                function (fileSystem) {
-                    root = fileSystem.root;
-                },
-                function () {
-                    def.reject();
-                }
-            );
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+                root = fileSystem.root;
+            }, function() {
+                def.reject();
+            });
         } catch (err) {
             def.reject();
         }
 
-        var remove_dir = function (entry) {
-            entry.removeRecursively(
-                function (parent) {
-                    def.resolve(parent);
-                },
-                function (err) {
-                    def.reject(err);
-                }
-            );
+        var remove_dir = function(entry) {
+            entry.removeRecursively(function(parent) {
+                def.resolve(parent);
+            }, function(err) {
+                def.reject(err);
+            });
         };
 
         // retrieve a file and truncate it
-        root.getDirectory(dirName, { create: false }, remove_dir, onFileSystemError);
+        root.getDirectory(dirName, {
+            create : false
+        }, remove_dir, onFileSystemError);
 
         return def.promise();
     };
 
-
-
     var downloadsQueue = $({});
 
-    $.downloadQueue = function (data) {
-        var jqXHR,
-            dfd = $.Deferred(),
-            promise = dfd.promise();
+    $.downloadQueue = function(data) {
+        var jqXHR, dfd = $.Deferred(), promise = dfd.promise();
 
         // run the actual query
         function doDownload(next) {
@@ -84,85 +68,81 @@ var DownloadModel = {};
 
                     try {
                         //console.debug(downloadData);
-                        var folderName = downloadData.folderName,
-                            fileName = downloadData.fileName,
-                            folderDir,
-                            fileDir,
-                            uri = encodeURI("http://" + appUser.username + ":" + appUser.password + "@www.atlas-cms.com" + downloadData.path),
-                            ft = new FileTransfer();
+                        var folderName = downloadData.folderName, fileName = downloadData.fileName, folderDir, fileDir, uri = encodeURI("http://" + appUser.username + ":" + appUser.password + "@www.atlas-cms.com" + downloadData.path), ft = new FileTransfer();
 
-                        fs.root.getDirectory(
-                            folderName,
-                            { create: true, exclusive: false },
-                            function (dirEntry) {
-   dirEntry.setMetadata(success2, fail2, { "com.apple.MobileBackup": 1 });
+                        fs.root.getDirectory(folderName, {
+                            create : true,
+                            exclusive : false
+                        }, function(dirEntry) {
+                            dirEntry.setMetadata(success2, fail2, {
+                                "com.apple.MobileBackup" : 1
+                            });
 
-                                function success2() {
-                                    console.log("The metadata was successfully set.");
-                                }
-
-                                function fail2() {
-                                    console.log("There was an error in setting the metadata");
-                                }
-
-                                folderDir = dirEntry;
-
-                                //Create File. somehow this needs to be done first before the download started
-                                folderDir.getFile(
-                                    fileName,
-                                    { create: true, exclusive: false },
-                                    function (fileEntry) {
-                                        fileDir = fileEntry;
-
-                                        //created the files hull needed for download. Save the path.
-                                        var fullpath = fileDir.fullPath.replace(fileName, "");
-
-                                        // now delete the file (for what ever reason)
-                                        fileDir.remove();
-                                        ft.download(
-                                              uri,
-                                              fullpath + fileName,
-                                              function (entry) {
-
-                                                  console.debug("Download success!" + entry.fullPath);
-
-                                                  //Set donotbackup Attribute for iCloud. This is needed by Apple
-
-                                                  //iOS Quirk
-                                                 //only the "com.apple.MobileBackup" extended attribute is supported. Set the value to 1 to NOT enable the file to be backed up by iCloud. Set the value to 0 to re-enable the file to be backed up by iCloud.
-
-                                                   entry.setMetadata(success, fail, { "com.apple.MobileBackup": 1 });
-
-                                                   function success() {
-                                                       console.log("The metadata was successfully set.");
-                                                   }
-
-                                                   function fail() {
-                                                       console.log("There was an error in setting the metadata");
-                                                   }
-
-
-                                                  download.resolve(entry);
-                                              },
-                                              function (error) {
-
-                                                  alert("Fehler beim Herunterladen. Zeitüberschreitung " + error.code);
-                                                  console.debug(error);
-                                                  download.reject(error);
-                                              },
-                                              true
-                                              );
-                                    },
-                                    function (error) {
-                                        download.reject(error);
-                                        // Base64.encode(appUser.username + ":" + appUser.password)
-                                    }
-                                );
-                            },
-                            function (error) {
-                                download.reject(error);
+                            function success2() {
+                                console.log("The metadata was successfully set.");
                             }
-                        );
+
+                            function fail2() {
+                                console.log("There was an error in setting the metadata");
+                            }
+
+                            folderDir = dirEntry;
+
+                            //Create File. somehow this needs to be done first before the download started
+                            folderDir.getFile(fileName, {
+                                create : true,
+                                exclusive : false
+                            }, function(fileEntry) {
+                                fileDir = fileEntry;
+
+                                //created the files hull needed for download. Save the path.
+                                var fullpath = fileDir.fullPath.replace(fileName, "");
+
+                                // now delete the file (for what ever reason)
+                                fileDir.remove();
+                                ft.download(uri, fullpath + fileName, function(entry) {
+
+                                    console.debug("Download success!" + entry.fullPath);
+
+                                    //Set donotbackup Attribute for iCloud. This is needed by Apple
+
+                                    //iOS Quirk
+                                    //only the "com.apple.MobileBackup" extended attribute is supported. Set the value to 1 to NOT enable the file to be backed up by iCloud. Set the value to 0 to re-enable the file to be backed up by iCloud.
+
+                                    entry.setMetadata(success, fail, {
+                                        "com.apple.MobileBackup" : 1
+                                    });
+
+                                    function success() {
+                                        console.log("The metadata was successfully set.");
+                                    }
+
+                                    function fail() {
+                                        console.log("There was an error in setting the metadata");
+                                    }
+
+
+                                    download.resolve(entry);
+                                }, function(error) {
+                                    console.debug(error.body);
+                                    if (error.body == "404 NOT FOUND") {
+                                        console.debug("404");
+                                        download.reject(error);
+                                    } 
+                                    else {
+                                        alert("Fehler beim Herunterladen. Zeitüberschreitung " + error.code);
+                                        console.debug(error);
+                                        download.reject(error);
+                                    }
+
+                                }, true);
+                            }, function(error) {
+                                download.reject(error);
+                                // Base64.encode(appUser.username + ":" + appUser.password)
+                            });
+                        }, function(error) {
+                            download.reject(error);
+                        });
                     } catch (err) {
                         download.reject(err);
                     }
@@ -177,17 +157,14 @@ var DownloadModel = {};
                 return download.promise()
             }
 
-            jqXHR = download(data)
-                .done(dfd.resolve)
-                .fail(dfd.reject)
-                .then(next, next);
+            jqXHR = download(data).done(dfd.resolve).fail(dfd.reject).then(next, next);
 
         }
 
         // queue our ajax request
         downloadsQueue.queue(doDownload);
 
-        promise.abort = function (statusText) {
+        promise.abort = function(statusText) {
 
             downloadsQueue.clearQueue();
 
@@ -196,5 +173,4 @@ var DownloadModel = {};
 
         return promise;
     }
-
 })(jQuery);
