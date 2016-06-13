7fb27b671f3a5fa414c270d35988de176dbe8a4b
bitwork GmbH <info@bitwork.net>
2016-06-13 13:19:57 +0200

    alte plugins entfernt

--- APP/plugins/com.contraterrene.LaunchFile/src/ios/LaunchFile.h
+++
@@ -1,7 +0,0 @@
-#import <Cordova/CDV.h>
-
-@interface LaunchFile : CDVPlugin <UIDocumentInteractionControllerDelegate>
-@property (strong) UIDocumentInteractionController *doccontroller;
-- (void)launchFile:(CDVInvokedUrlCommand*)command;
-
-@end
--- APP/plugins/com.contraterrene.LaunchFile/www/LaunchFile.js
+++
@@ -1,7 +0,0 @@
-exec = require('cordova/exec')
-LaunchFile = function(str, callback){
-        exec(callback, function(err) {
-            callback('File name not supplied');
-        }, "LaunchFile", "launchFile", [str]);
-};
-module.exports = LaunchFile;
